f573bfbbef7fe8155cf4a454ab0cf95e4f911085
ribbb <iiro.lindstrom1@gmail.com>
2020-08-11 14:58:14 +0300

    Added image to landingpage

--- src/components/landing_page.jsx
+++ src/components/landing_page.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-
+import logo from "../images/image001.png";
 class LandingPage extends Component {
   //state = {  }
   render() {
@@ -7,28 +7,39 @@ class LandingPage extends Component {
       width: "70%",
       margin: "0 auto",
       paddingTop: "5%",
+      paddingBottom: "20px",
     };
     let buttonSyle = {
-      margin: "0",
-      position: "absolute",
-      left: "50%",
+      display: "block",
+      marginLeft: "auto",
+      marginRight: "auto",
+      width: "50%",
+    };
+    let center = {
+      display: "block",
+      marginLeft: "auto",
+      marginRight: "auto",
+      width: "50%",
+      minWidth: "300px",
     };
     return (
       <div style={style}>
-        <h3>
+        <img src={logo} style={center}></img>
+
+        <p>
           Ruokavaliolla on keskeinen merkitys terveyden ja hyvinvoinnin
           ylläpitämisessä. Testaa ruokavaliosi laatu vastaamalla seuraaviin
           kysymyksiin. Kysymykset on kehitetty DEHKON 2D-hankkeessa, ja niiden
           pohjalta laskettu RUOKAVALIOINDEKSI StopDia-hankkeessa.{" "}
-        </h3>
+        </p>
         <br />
         <br />
-        <h3>
+        <p>
           RUOKAVALIOINDEKSI kuvaa sitä, miten hyvin nykyinen tapasi syödä on
           linjassa ravitsemussuositusten ja tyypin 2 diabetekselta suojaavan
           ruokavalion kanssa. Kysymykset on jaettu seitsemään osioon.
           Vastattuasi kaikkiin kysymyksiin saat arvion ruokavaliosi laadusta.
-        </h3>
+        </p>
         <br />
         <button
           className="btn btn-success btn-lg"
